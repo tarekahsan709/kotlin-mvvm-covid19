30b4bf9f405696610bbb16934a9d69235461b418
rizmaulana <rizmaulana@live.com>
2020-03-22 11:46:08 +0700

    Add indonesia per country entry point

--- app/src/main/java/id/rizmaulana/covid19/ui/adapter/ItemTypeFactory.kt
+++ app/src/main/java/id/rizmaulana/covid19/ui/adapter/ItemTypeFactory.kt
@@ -5,19 +5,23 @@ import id.rizmaulana.covid19.ui.adapter.viewholders.*
 import id.rizmaulana.covid19.ui.base.BaseViewItem
 
 abstract class ItemTypeFactory {
-    abstract fun onCreateViewHolder(containerView: View, viewType: Int): BaseViewHolder<out BaseViewItem>
+    abstract fun onCreateViewHolder(
+        containerView: View,
+        viewType: Int
+    ): BaseViewHolder<out BaseViewItem>
 }
 
-class ItemTypeFactoryImpl: ItemTypeFactory() {
+class ItemTypeFactoryImpl : ItemTypeFactory() {
     override fun onCreateViewHolder(
         containerView: View,
         viewType: Int
     ): BaseViewHolder<out BaseViewItem> {
-        return when(viewType) {
+        return when (viewType) {
             DailyItemViewHolder.LAYOUT -> DailyItemViewHolder(containerView)
             OverviewItemViewHolder.LAYOUT -> OverviewItemViewHolder(containerView)
             TextItemViewHolder.LAYOUT -> TextItemViewHolder(containerView)
             PinnedItemViewHolder.LAYOUT -> PinnedItemViewHolder(containerView)
+            PerCountryViewHolder.LAYOUT -> PerCountryViewHolder(containerView)
             LocationItemViewHolder.LAYOUT -> LocationItemViewHolder(containerView)
             LoadingStateItemViewHolder.LAYOUT -> LoadingStateItemViewHolder(containerView)
             ErrorStateItemViewHolder.LAYOUT -> ErrorStateItemViewHolder(containerView)
